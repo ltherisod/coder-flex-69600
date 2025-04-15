4335b6529e62d52684f4abf84458c000dc893b49
ltherisod <lauratherisod@gmail.com>
2025-04-15 19:34:01 -0300

    navegacion parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar"
 import ItemListContainer from "./components/ItemListContainer"
-import { Button } from 'react-bootstrap';
 import NavbarBootstrap from './components/NavbarBootstrap';
 import ItemDetailContainer from './components/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// import FetchCountry from './examples/FetchCountry';
-// import FetchData from './examples/FetchData';
-// import { withLogging } from './hocs/withLogging';
-// import Hello from './examples/Hello';
-
-// import Button from './examples/Button'
 function App() {
-console.log('soy App')
-// const HelloConLogging= withLogging(Hello)
-  return (
-<>
 
+  return (
+<BrowserRouter>
 <NavbarBootstrap/>
-<ItemListContainer greeting='Bienvenidos a mi App 🙌'/>
-<ItemDetailContainer/>
-{/* <FetchCountry/> */}
-{/* <FetchData/> */}
-{/* <HelloConLogging/> */}
-{/* <Hello/> */}
-</>
+<Routes>
+  <Route path='/' element={<ItemListContainer greeting='Bienvenidos a mi App 🙌'/>}/>
+  <Route path='/item' element={<ItemDetailContainer/>}/>
+</Routes>
+
+</BrowserRouter>
   )
 }
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,17 +1,15 @@
-import ItemCount from "./ItemCount"
 import {getProducts} from '../mock/asyncService'
 import { useEffect, useState } from "react"
 import ItemList from "./ItemList"
 const ItemListContainer = ({greeting}) => {
   const [data, setData]= useState([])
-    console.log('ItemListContainer')
-  
+ 
     useEffect(()=>{
       getProducts()
       .then((res)=> setData(res))
       .catch((error)=> console.log(error))
     },[])
-    console.log(data)
+   
     return(
         <div>
           <h1>{greeting}</h1>
